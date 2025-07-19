e18e7e5ccf1d5e62b2ad3855864859b16bb5d697
Johanna Nguyen <johannatnguyen@gmail.com>
2025-07-18 21:30:25 -0700

    splt classes into a different file

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sys
 import requests
 from io import BytesIO
 from main import read_dataset, pokemon_picker
+from objects import Pokeball, Ditch
 
 # === Constants ===
 WIDTH = 800
@@ -27,65 +28,6 @@ pokeball_img = pygame.transform.scale(pokeball_img, (45, 45))
 ditch_img = pygame.image.load("./assets/ditch.png").convert_alpha()
 ditch_width, ditch_height = 100, 80
 ditch_img = pygame.transform.scale(ditch_img, (ditch_width, ditch_height))
-
-
-class Pokeball:
-    def __init__(self, image):
-        self.image = image
-        self.x = 0
-        self.y = 10
-        self.w = image.get_width()
-        self.h = image.get_height()
-        self.h_speed = 6
-        self.v_speed = 7
-        self.direction = 1
-        self.dropping = False
-        self.dropped = False
-
-    def update(self):
-        if not self.dropping:
-            self.x += self.h_speed * self.direction
-            if self.x <= 0 or self.x + self.w >= WIDTH:
-                self.direction *= -1
-        else:
-            self.y += self.v_speed
-            if self.y + self.h >= HEIGHT:
-                self.y = HEIGHT - self.h
-                self.dropped = True
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-    def drop(self):
-        self.dropping = True
-
-    def get_rect(self):
-        return pygame.Rect(self.x, self.y, self.w, self.h)
-
-
-class Ditch:
-    def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-        self.rect = pygame.Rect(x, y, ditch_width, ditch_height)
-
-        # Widened perfect zone: middle 60%
-        margin = ditch_width * 0.20  # 20% margin each side
-        zone_width = ditch_width * 0.60
-        self.perfect_zone = pygame.Rect(
-            x + margin, y, zone_width, ditch_height
-        )
-
-    def draw(self, surface, debug=False):
-        surface.blit(self.image, (self.x, self.y))
-        if debug:
-            overlay = pygame.Surface((self.perfect_zone.width, self.perfect_zone.height), pygame.SRCALPHA)
-            overlay.fill((0, 255, 0, 100))  # translucent green
-            surface.blit(overlay, (self.perfect_zone.x, self.perfect_zone.y))
-
-    def is_perfect_landing(self, pokeball_rect):
-        return self.perfect_zone.contains(pokeball_rect)
 
 
 # Create ditches
